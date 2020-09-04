439461d0ed67fa50ecb658e3171a0067e805cd8a
Rafael Gábrio <rafaelgabriosi@gmail.com>
2020-09-04 18:05:24 -0300

    CSS Global concluído

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      
+      <button type="button">
+        Clica em mim
+      </button>
     </div>
     
   );
